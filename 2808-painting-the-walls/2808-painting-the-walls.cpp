af4147fb0401273499b926588991d5038273f5c9
Dhiraj_jadhav <76464709+Dhirajjadhadhav@users.noreply.github.com>
2023-10-14 23:43:51 +0530

    Time: 183 ms (33.08%), Space: 116.2 MB (24.65%) - LeetHub

---
+++ 2808-painting-the-walls/2808-painting-the-walls.cpp
@@ -0,0 +1,23 @@
+class Solution {
+private:
+    int solve(vector<int>&cost, vector<int>&time, int i, int bank, vector<vector<int>>&dp)    {
+
+            if(bank<=0)
+                return 0;
+            if(i>=cost.size())
+                return 1e9;
+            if(dp[i][bank]!=-1)
+                return dp[i][bank];
+
+            int notTake = solve(cost, time, i+1, bank, dp);
+            int take = cost[i]+solve(cost, time, i+1,bank-time[i] -1, dp);
+            dp[i][bank] = min(notTake, take);   
+            return dp[i][bank];
+        }
+public:
+    int paintWalls(vector<int>& cost, vector<int>& time) {
+        
+        vector<vector<int>>dp(time.size()+1, vector<int>(time.size()+1, -1));
+        return solve(cost, time, 0, time.size(), dp);
+    }   
+};
