7ef004d3877c7940b14cc50555b7f993e585e29c
Dhiraj_jadhav <76464709+Dhirajjadhadhav@users.noreply.github.com>
2023-10-29 18:40:46 +0530

    Time: 35 ms (60%), Space: 32.9 MB (10%) - LeetHub

---
+++ 3163-subarrays-distinct-element-sum-of-squares-i/3163-subarrays-distinct-element-sum-of-squares-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+ 
+public:
+    int sumCounts(vector<int>& nums) {
+        int ans = 0;
+        for(int i = 0; i<nums.size(); ++i)
+        {
+            unordered_map<int, int>mp;
+            int count = 0;
+            for(int j = i; j<nums.size(); ++j)
+            {
+                mp[nums[j]]++;
+                if(mp[nums[j]] == 1)count++;
+                ans+= count*count;
+            }
+        }
+        return ans;
+    }
+};
